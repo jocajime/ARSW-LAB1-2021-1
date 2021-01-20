870b1462f23613ca36f2d8cb6dd126be04f464c5
joel carvajal <joel.carvajal@mail.escuelaing.edu.co>
2021-01-20 09:55:57 -0500

    parte 1 incompleta

---
+++ src/main/java/edu/eci/arsw/threads/CountThread.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.threads;
+
+/**
+ *
+ * @author hcadavid
+ */
+public class CountThread extends Thread {
+    int A;
+    int B;
+    String nombre;
+
+    public CountThread(int a, int b, String nombre){
+        this.A = a;
+        this.B = b;
+        this.nombre = nombre;
+    }
+
+    @Override
+    public void run() {
+        for (int i = A;i<=B;i++){
+            System.out.println(i + " " + nombre);
+        }
+    }
+}
